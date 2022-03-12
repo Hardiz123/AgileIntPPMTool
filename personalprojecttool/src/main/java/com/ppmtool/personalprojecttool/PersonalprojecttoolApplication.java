070591bf117daed64383cba94d6cbcdc80a8f795
Hardik2646 <hardik.nandwani@getfareye.com>
2022-03-12 20:05:57 +0530

    first commit

---
+++ personalprojecttool/src/main/java/com/ppmtool/personalprojecttool/PersonalprojecttoolApplication.java
@@ -0,0 +1,13 @@
+package com.ppmtool.personalprojecttool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalprojecttoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalprojecttoolApplication.class, args);
+	}
+
+}
---
+++ personalprojecttool/src/test/java/com/ppmtool/personalprojecttool/PersonalprojecttoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppmtool.personalprojecttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalprojecttoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
