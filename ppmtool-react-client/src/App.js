33d52fd1702dd16bb11290f78358aa02cb46ec00
Hardik2646 <hardik.nandwani@getfareye.com>
2022-03-14 02:44:25 +0530

    added components

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import AddProject from './components/project/AddProject';
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Route exact path="/dashboard" component={Dashboard}/>
+      <Route exact path="/addProject" component={AddProject}/>
     </div>
+    </Router>
   );
 }
 
